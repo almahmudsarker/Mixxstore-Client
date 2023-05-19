4e0174a6a3815d011f17a127460ecb8319e75317
almahmudsarker <almahmuds427@gmail.com>
2023-05-19 22:30:18 +0600

    shop category added

--- src/pages/Home/Home/Payments.jsx
+++ src/pages/Home/Home/Payments.jsx
@@ -2,9 +2,28 @@ import React from 'react';
 
 const Payments = () => {
     return (
-        <div>
-            <h1>This is payments</h1>
-        </div>
+      <div className="flex w-60">
+        <img
+          className="mr-4"
+          src="https://i.ibb.co/nw8y1WN/pngegg.png"
+          alt=""
+        />
+        <img
+          className="mr-4"
+          src="https://i.ibb.co/tZdjyZS/pngegg-1.png"
+          alt=""
+        />
+        <img
+          className="mr-5"
+          src="https://i.ibb.co/pZzdNR2/pngegg-2.png"
+          alt=""
+        />
+        <img
+          className="mr-5"
+          src="https://i.ibb.co/QDLBX06/pngegg-3.png"
+          alt=""
+        />
+      </div>
     );
 };
 
