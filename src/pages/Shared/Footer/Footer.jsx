cc4e155fec1d728d06b8801e54ff66a813199deb
almahmudsarker <almahmuds427@gmail.com>
2023-05-19 17:28:25 +0600

    footer added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -20,11 +20,11 @@ const Footer = () => {
           <a className="link link-hover">Advertisement</a>
         </div>
         <div>
-          <span className="footer-title">Company</span>
+          <span className="footer-title">Address</span>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <a className="link link-hover"> Press kits</a>
+          <a className="link link-hover">Social Media</a>
         </div>
         <div>
           <span className="footer-title">Legal</span>
