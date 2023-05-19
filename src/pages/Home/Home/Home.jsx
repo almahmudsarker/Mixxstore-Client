9511bc4399581eafed713d5c83a39b2a165061d0
almahmudsarker <almahmuds427@gmail.com>
2023-05-19 18:58:14 +0600

    gallery added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import useTitle from "../../../hooks/useTitle";
 import Banner from "../Banner/Banner";
+import Gallery from "../Gallery/Gallery";
 
 const Home = () => {
     useTitle("Home");
     return (
         <div>
             <Banner />
+            <Gallery />
             <h2>This is Home</h2>
         </div>
     );
